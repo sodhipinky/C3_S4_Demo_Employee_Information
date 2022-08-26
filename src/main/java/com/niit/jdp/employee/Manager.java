cfdd0ca238db49d5be5aae3e728f3cb93f166dc2
Pinky Sodhi <pinky.sodhi@niit.com>
2022-08-26 15:00:43 +0530

    added parameterized constructor to 'Manager.java'

--- src/main/java/com/niit/jdp/employee/Manager.java
+++ src/main/java/com/niit/jdp/employee/Manager.java
@@ -1,7 +1,15 @@
 package com.niit.jdp.employee;
 
+import java.time.LocalDate;
+
 public class Manager extends Employee {
     private int teamSize;
     private int ratingByTeam;
+
+    public Manager(String name, int age, LocalDate dateOfBirth, String address, double salary, int teamSize, int ratingByTeam) {
+        super(name, age, dateOfBirth, address, salary);
+        this.teamSize = teamSize;
+        this.ratingByTeam = ratingByTeam;
+    }
 
 }
