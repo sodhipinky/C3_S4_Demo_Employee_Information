8202caa3f4f2ba4754cf958e1c30031cc3c442a7
Pinky Sodhi <pinky.sodhi@niit.com>
2022-08-26 15:08:10 +0530

    created the object of 'Manager' class

--- src/main/java/com/niit/jdp/employee/ManagerImpl.java
+++ src/main/java/com/niit/jdp/employee/ManagerImpl.java
@@ -1,7 +1,18 @@
 package com.niit.jdp.employee;
 
+import java.time.LocalDate;
+
 public class ManagerImpl {
     public static void main(String[] args) {
+        Manager manager = new Manager(
+                "Aditya",
+                23,
+                LocalDate.of(1999, 05, 20),
+                "Pune",
+                1_00_000.0,
+                5,
+                5);
 
+    }
     }
 }
