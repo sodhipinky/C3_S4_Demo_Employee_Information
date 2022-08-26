183ced588cb4c79b3fb843ed35cd2cfa6ab3e3c0
Pinky Sodhi <pinky.sodhi@niit.com>
2022-08-26 15:12:38 +0530

    displayed the details of manager object

--- src/main/java/com/niit/jdp/employee/ManagerImpl.java
+++ src/main/java/com/niit/jdp/employee/ManagerImpl.java
@@ -12,7 +12,12 @@ public class ManagerImpl {
                 1_00_000.0,
                 5,
                 5);
-
-    }
+        System.out.println("Name = " + manager.getName());
+        System.out.println("Age = " + manager.getAge());
+        System.out.println("Date Of Birth = " + manager.getDateOfBirth());
+        System.out.println("Address = " + manager.getAddress());
+        System.out.println("Salary = " + manager.getSalary());
+        System.out.println("Team Size = " + manager.getTeamSize());
+        System.out.println("Rating By Team = " + manager.getRatingByTeam());
     }
 }
