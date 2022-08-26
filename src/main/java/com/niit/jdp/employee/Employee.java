93886d328dd16c7001d61090d673bc75a5aef6ed
Pinky Sodhi <pinky.sodhi@niit.com>
2022-08-26 14:48:25 +0530

    added private fields to 'Employee.java'

--- src/main/java/com/niit/jdp/employee/Employee.java
+++ src/main/java/com/niit/jdp/employee/Employee.java
@@ -1,4 +1,12 @@
 package com.niit.jdp.employee;
 
+import java.time.LocalDate;
+
 public class Employee {
+    private String name;
+    private int age;
+    private LocalDate dateOfBirth;
+    private String address;
+    private double salary;
+
 }
